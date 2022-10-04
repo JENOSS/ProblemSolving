bf231c95f0ec730094cd515b0307901fde07e4e4
PARK JIN WOO <nari1539@gmail.com>
2022-10-04 23:17:45 +0900

    [릿코드]
    [ easy ] [ 5 ] Build Array from Permutation

---
+++ LeetCode/easy/[easy] Build Array Permutation.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+/**
+ * [easy] [5] Build Array from Permutation
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] buildArray(int[] nums) {
+        int[] result = new int[nums.length];
+        for(int i = 0; i < nums.length; i++) result[i] = nums[nums[i]];
+        return result;
+    }
+
+}
+
+
